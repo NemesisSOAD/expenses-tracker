c04822d6a3b1aa1673a775107ef82bd760104c25
jDmc <dmitriy.chepurnoy@gmail.com>
2012-08-26 20:56:45 +0300

    Added a couple of validation tests for expense.

--- public/test/spec/models/ExpenseSpec.js
+++ public/test/spec/models/ExpenseSpec.js
@@ -2,20 +2,28 @@ describe('Model :: Expense', function() {
 		
 		beforeEach(function() {
 			var that = this
-				flag = false;
+				done = false;
 
 			require([
 				'models/Expense',
-				'utils'
+				'utils',
+				'backboneValidation',
+				'memoryStorage'
 			], function(Expense, utils) {
+				that.Expense = Expense;
 				that.expense = new Expense();
 				that.utils = utils;
 
-				flag = true;
+				Backbone.Validation.configure({
+					forceUpdate: true
+				});
+				_.extend(Backbone.Model.prototype, Backbone.Validation.mixin);
+
+				done = true;
 			});
 
 			waitsFor(function() {
-				return flag;
+				return done;
 			});
 		});
 
@@ -28,6 +36,61 @@ describe('Model :: Expense', function() {
 				expect(this.expense.get('dateCreated').getTime()).toEqual(jasmine.any(Number));
 			});
 
+		});
+
+		describe('when saving', function(){
+
+			beforeEach(function() {
+				this.eventSpy = sinon.spy();
+				this.done = false;
+				this.isDone = function(){return done};
+			});
+
+			it('should be invalid when name and description are empty', function() {
+
+				var self = this;
+
+				this.expense.on('validated', this.eventSpy);
+
+				this.expense.isValid(true);
+
+				this.expense.on('validated', function(isValid, model, errors){
+					
+					expect(self.eventSpy).toHaveBeenCalledOnce();
+					expect(self.eventSpy).toHaveBeenCalledWith(
+						false,
+						self.expense,
+						{
+							name:			'Name is required',
+							description:	'Please provide description (10 symbols at least)' 
+						}
+					);
+					self.done = true;
+				});
+
+				waitsFor(this.isDone);
+			});
+
+			it('should be valid when name and description are not empty', function() {
+
+				var self = this;
+
+				this.expense.on('validated:valid', this.eventSpy);
+
+				this.expense.set({
+					name:			'Name',
+					description:	'NotEmptyDescription'
+				});
+
+				this.expense.on('validated:valid', function(model){
+					
+					expect(self.eventSpy).toHaveBeenCalledOnce();
+					expect(self.eventSpy).toHaveBeenCalledWith(self.expense);
+					self.done = true;
+				});
+
+				waitsFor(this.isDone);
+			});
 		});
 
 });
