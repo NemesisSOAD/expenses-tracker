df9a732bbe10d76928c7a40f057028c28fadb3eb
jDmc <dmitriy.chepurnoy@gmail.com>
2012-10-03 13:54:10 +0300

    Added connection string to mongohq DB

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {
 	mongoDb: {
-		connectionString: 'jDmc:admin@ds037007.mongolab.com:37007/expenses'
+		// connectionString: 'jDmc:admin@ds037007.mongolab.com:37007/expenses'
+		connectionString: 'nodejitsu:d69a5757718edee8c7b0b8632f7b389c@alex.mongohq.com:10035/nodejitsudb814410890825'
 	},
 	app: {
 		port: 3000
